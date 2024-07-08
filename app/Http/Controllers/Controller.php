488387ab7a89686f8cb48286ae4027e221957f41
muhammad-alaa99 <muhammedalaa1999@gmail.com>
2024-07-09 01:32:31 +0300

    update verificationEmail

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Mail\VerificationEmail;
+use Illuminate\Support\Facades\Mail;
 
 abstract class Controller
 {
-    //
+
+public function sendVerificationEmail()
+{
+    $details = [
+        'title' => 'Mail from MyApp',
+        'body' => 'This is for testing email using smtp.'
+    ];
+
+    Mail::to('laravel@example.com')->send(new VerificationEmail($details));
+
+    return 'Email sent successfully';
+}
+
 }
